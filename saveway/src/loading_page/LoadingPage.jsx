8ee1a8615c0a86d9f8cfd474f22aba34459c5192
sanananan3 <jjy830104@g.skku.edu>
2024-08-01 16:29:56 +0900

    porthole component adding

--- saveway/src/loading_page/LoadingPage.jsx
+++ saveway/src/loading_page/LoadingPage.jsx
@@ -3,19 +3,26 @@ import {Canvas} from "@react-three/fiber";
 import {LoadInside} from "./LoadInside";
 import './loadingpage.css';
 import City from "./component/City";
-import StreetAsset from './component/StreetAsset';
-import StreetAsset2 from './component/StreetAsset2';
+import Tubular from './component/Tubular';
 import RubberCone from './component/RubberCone';
-import { StringKeyframeTrack } from "three";
+import { PortHole_1 } from "./component/PortHole_1";
+import { PortHole_2 } from "./component/PortHole_2";
+import { PortHole_3 } from "./component/PortHole_3";
+import { PortHole_4 } from "./component/PortHole_4";
 
 export default function LoadingPage(){
 
     return (
     <Canvas camera={{fov:90, position: [-25,50,75]}}>
+
         <LoadInside/>
         <City />
         <RubberCone />
-        <StreetAsset2 />
+        <Tubular />
+        <PortHole_2 />
+        <PortHole_3 />
+        
+
     </Canvas>
     )
 

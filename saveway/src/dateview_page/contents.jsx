6ff779fe249caeb8e70c9b131c9005ecfa6a9525
seoyoung <psy20116@gmail.com>
2024-08-13 19:50:24 +0900

    second

--- saveway/src/dateview_page/contents.jsx
+++ saveway/src/dateview_page/contents.jsx
@@ -1,25 +1,63 @@
+
+// import React from 'react';
+// import './contents.css';
+// import personIcon from './../assets/person_icon.png';
+
+// const Contents = ({ date, userName, photoCount, isEven }) => {
+//     // isEven에 따라 배경색 결정
+//     const backgroundColor = isEven ? 'rgba(128, 128, 128, 0.7)' : 'rgba(189, 154, 154, 0.4)';
+
+//     return (
+//         <div className='contents_container' style={{ backgroundColor }}>
+//             <div className='select_box'>
+//                 <div className='date_box'>{date}</div>
+//                 <input type="checkbox" name="select" value="select" />
+//             </div>
+//             <div className='user'>
+//                 <img src={personIcon} alt="User Icon" className='user_icon' />
+//                 {userName}
+//             </div>
+//             <div className='photo_num'>
+//                 <p className='star'>☆</p>
+//                 <p className="number">{photoCount}</p>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export { Contents };
+
 import React from 'react';
 import './contents.css';
-import personIcon from './../assets/person_icon.png'; 
+import personIcon from './../assets/person_icon.png';
 
-const Contents = () => {
+const Contents = ({ id, date, userName, photoCount, isEven, onSelect, isSelected }) => {
+    // isEven에 따라 배경색 결정
+    const backgroundColor = isEven ? 'rgba(128, 128, 128, 0.7)' : 'rgba(189, 154, 154, 0.4)';
 
     return (
-        <div className='contents_container'>
+        <div className='contents_container' style={{ backgroundColor }}>
             <div className='select_box'>
-                <div className='date_box'>2024-08-13</div>
-                <input type="checkbox" name="select" value="select" />
+                <div className='date_box'>{date}</div>
+                <input
+                    type="checkbox"
+                    name="select"
+                    value="select"
+                    checked={isSelected}
+                    onChange={() => onSelect(id)}
+                />
             </div>
             <div className='user'>
                 <img src={personIcon} alt="User Icon" className='user_icon' />
-                user_name
+                {userName}
             </div>
             <div className='photo_num'>
                 <p className='star'>☆</p>
-                <p>3</p>
+                <p className="number">{photoCount}</p>
             </div>
         </div>
     );
 };
 
 export { Contents };
+
--- saveway/src/dateview_page/delete.jsx
+++ saveway/src/dateview_page/delete.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import delete_icon from './../assets/delete.png';
 import './delete.css';
 
-const Delete = () => {
+const Delete = ({ onDelete }) => {
 
   return (
-    <div className='delete'>
+    <div className='delete' onClick={onDelete}>
       <img src={delete_icon} alt='delete' />
     </div>
   );
 };
 
-export {Delete};
+export { Delete };

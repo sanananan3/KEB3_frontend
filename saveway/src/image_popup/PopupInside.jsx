81f5de73ca23b377aa5e5cb3177700f231cbae12
sanananan3 <jjy830104@g.skku.edu>
2024-08-14 15:16:12 +0900

    jiyeon's api

---
+++ saveway/src/image_popup/PopupInside.jsx
@@ -0,0 +1,19 @@
+import {React} from 'react';
+import './popup.css';
+
+
+export const PopupInside = () => {
+
+    return (
+        <div className = "ImageWrap" >
+            <div className = "ImageContent">
+
+                
+            </div>
+            
+
+        </div>
+
+    );
+
+};
